d65a8a4bf6550e1fcb895702af0c19bc988e8ac0
Akmal Sab <akmalsabri@outlook.com>
2021-10-26 12:48:37 +0800

    update reset password form

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <h3 class="card-header text-center fw-bold">{{ __('Reset Password') }}</h3>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,10 +17,10 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="mb-3 row">
+                            <label for="email" class="col-lg-4 col-md-12 col-form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-lg-8 col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -31,13 +31,18 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <button type="submit" class="col-12 btn btn-primary">
+                                {{ __('Send Password Reset Link') }}
+                                </button>  
+                            </div> 
+                            <div class="col-6"> 
+                                <button type="reset" class="col-12 btn btn-warning">
+                                    {{ __('Reset') }}
+                                </button>                    
+                            </div>           
+                        </div>                        
                     </form>
                 </div>
             </div>
